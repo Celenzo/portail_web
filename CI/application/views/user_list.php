aff0a93d8b9baad383036b12b049a81fe8ce0fe9
Celenzo <so6master@gmail.com>
2016-09-12 12:06:44 +0200

    Maintenant ?
    stp ?

---
+++ CI/application/views/user_list.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<meta charset='UTF-8'/>
+		<title>Créer un compte</title>
+		<link rel="stylesheet" href="<?php echo site_url('css/style.css');?>">
+	</head>
+	<body>
+		<ul  id="nav_list">
+			<li><a href="<?php echo site_url('index.php/home/')?>">Retour sur l'accueil</a></li>
+			<li><a href="<?php echo site_url('index.php/home/register')?>">Créer un compte</a></li>
+		</ul>
+	</body>
+</html>
